d3027bb8a4aece660163bf118f900b1f5a1079a0
Nader Hosseini Naghavi <naderhosseininaghavi@Naders-MacBook-Pro.local>
2024-08-11 15:59:29 -0400

    new

---
+++ R/function1.R
@@ -0,0 +1,14 @@
+#' Title of your function
+#' 
+#' @description A short description of what your function does.
+#' @param arg1 Description of the first argument.
+#' @param arg2 Description of the second argument.
+#' @return Description of what the function returns.
+#' @export
+#'
+#' 
+function1 <- function(arg1, arg2) {
+    # Your function code
+    print(arg1)
+    print(arg2)
+}
